fa69090d45b248286c8f256a0c0f25c9135ea968
moritzfelipe <stellmacher@gmail.com>
2019-05-06 17:30:25 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,41 +1,40 @@
 const contractSource = `
   contract MemeVote =
 
-    record meme =
-      { creatorAddress : address,
-        url            : string,
-        name           : string,
-        voteCount      : int }
+      record meme =
+        { creatorAddress : address,
+          url            : string,
+          name           : string,
+          voteCount      : int }
 
-    record state =
-      { memes      : map(int, meme),
-        memesLength : int }
+      record state =
+        { memes      : map(int, meme),
+          memesLength : int }
 
-    function init() =
-      { memes = {},
-        memesLength = 0 }
+      function init() =
+        { memes = {},
+          memesLength = 0 }
 
-    public function getMeme(index : int) : meme =
-      switch(Map.lookup(index, state.memes))
-        None    => abort("There was no meme with this index registered.")
-        Some(x) => x
+      public function getMeme(index : int) : meme =
+        switch(Map.lookup(index, state.memes))
+          None    => abort("There was no meme with this index registered.")
+          Some(x) => x
 
-    public stateful function registerMeme(url' : string, name' : string) =
-      let meme = { creatorAddress = Call.caller, url = url', name = name', voteCount = 0}
-      let index = getMemesLength() + 1
-      put(state{ memes[index] = meme, memesLength = index })
+      public stateful function registerMeme(url' : string, name' : string) =
+        let meme = { creatorAddress = Call.caller, url = url', name = name', voteCount = 0}
+        let index = getMemesLength() + 1
+        put(state{ memes[index] = meme, memesLength = index })
 
-    public function getMemesLength() : int =
-      state.memesLength
+      public function getMemesLength() : int =
+        state.memesLength
 
-    public stateful function voteMeme(index : int) =
-      let meme = getMeme(index)
-      Chain.spend(meme.creatorAddress, Call.value)
-      let updatedVoteCount = meme.voteCount + Call.value
-      let updatedMemes = state.memes{ [index].voteCount = updatedVoteCount }
-      put(state{ memes = updatedMemes })
+      public stateful function voteMeme(index : int) =
+        let meme = getMeme(index)
+        Chain.spend(meme.creatorAddress, Call.value)
+        let updatedVoteCount = meme.voteCount + Call.value
+        let updatedMemes = state.memes{ [index].voteCount = updatedVoteCount }
+        put(state{ memes = updatedMemes })
 `;
-
 const contractAddress ='ct_2qcqwwXmfLmZ3a18yvnv4p8ta9HGoyHRjCDvPMvyAqkuMRwzPD';
 var client = null;
 var memeArray = [];
@@ -58,7 +57,6 @@ window.addEventListener('load', async () => {
   const calledGet = await contract.call('getMemesLength', [], {callStatic: true}).catch(e => console.error(e));
   console.log('calledGet', calledGet);
 
-  //Make another call to decode the data received in first call
   const decodedGet = await calledGet.decode().catch(e => console.error(e));
   console.log('decodedGet', decodedGet);
 
