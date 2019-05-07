fe97595cf9f3c94c63c9ddd77d93bc29e563a5cf
moritzfelipe <stellmacher@gmail.com>
2019-05-07 08:40:58 +0200

    Vote and register functionality added

--- index.js
+++ index.js
@@ -56,6 +56,14 @@ async function callStatic(func, args) {
   return decodedGet;
 }
 
+async function contractCall(func, args, value) {
+  const contract = await client.getContractInstance(contractSource, {contractAddress});
+  const calledSet = await contract.call(func, args, {amount: value}).catch(e => console.error(e));
+
+  return calledSet;
+}
+
+
 window.addEventListener('load', async () => {
   $("#loader").show();
 
@@ -80,17 +88,28 @@ window.addEventListener('load', async () => {
 });
 
 jQuery("#memeBody").on("click", ".voteBtn", async function(event){
+  $("#loader").show();
+
   const value = $(this).siblings('input').val();
   const dataIndex = event.target.id;
+
+  await contractCall('voteMeme', [dataIndex], value);
+
   const foundIndex = memeArray.findIndex(meme => meme.index == dataIndex);
   memeArray[foundIndex].votes += parseInt(value, 10);
+
   renderMemes();
+
+  $("#loader").hide();
 });
 
 $('#registerBtn').click(async function(){
-  var name = ($('#regName').val()),
+  $("#loader").show();
+
+  const name = ($('#regName').val()),
       url = ($('#regUrl').val());
 
+  await contractCall('registerMeme', [url, name], 0);
 
   memeArray.push({
     creatorName: name,
@@ -100,4 +119,6 @@ $('#registerBtn').click(async function(){
   })
 
   renderMemes();
+  
+  $("#loader").hide();
 });
