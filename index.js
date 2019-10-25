d4b5b5a82e957fffe7699311c9d9a54772dc9e66
moritzfelipe <stellmacher@gmail.com>
2019-10-25 09:04:07 +0200

    Added payable to contract

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ const contractSource = `
 `;
 
 //Address of the meme voting smart contract on the testnet of the aeternity blockchain
-const contractAddress = 'ct_2rKDQqn4JcoPGWKq9Chtx3Dwq6hvfXJ2rsT7VepRnZXTyQh1zY';
+const contractAddress = 'ct_PYLcfDaSYBGS2GeZEeVCnsWYWDpdfYnH8uJz7Pdd6ASTYngyP';
 //Create variable for client so it can be used in different functions
 var client = null;
 // Global contract object
